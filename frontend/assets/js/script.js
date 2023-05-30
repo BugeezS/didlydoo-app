49e7fb111a33a768048ff2cb2c580cb97e16a97f
will1490 <willseghers@gmail.com>
2023-05-30 10:56:47 +0200

    Correction toggleButton

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,3 +1,11 @@
 import { testFetch } from "./testfetch.js";
+import { hiddenFormDisplayForm } from "./toggleButton.js";
+
+
 
 testFetch();
+
+
+const buttonToggle = document.getElementById('toggle');
+
+buttonToggle.addEventListener('click', hiddenFormDisplayForm);
--- frontend/assets/js/toggleButton.js
+++ frontend/assets/js/toggleButton.js
@@ -1,48 +1,11 @@
-const buttonToggle = document.querySelector('.toggle');
-const form = document.querySelector('.add-events');
 
-export function hideFormDisplayEvent() {
-  form.style.display = 'none';
-  console.log("hello");
-
-  function display() {
-    const author = document.getElementById('add-events__author').value;
-    const name = document.getElementById('add-events__name').value;
-    const description = document.getElementById('add-events__description').value;
-    const date = document.getElementById('add-events__date').value;
-
-    const eventContainer = document.createElement('div');
-    eventContainer.classList.add('event-container');
-
-    const closeButton = document.createElement('span');
-    closeButton.classList.add('close-button');
-    closeButton.textContent = '✖';
-
-    const eventAuthor = document.createElement('span');
-    const eventName = document.createElement('span');
-    const eventDescription = document.createElement('textarea');
-    const eventDate = document.createElement('span');
-
-    eventAuthor.textContent = author;
-    eventName.textContent = name;
-    eventDescription.textContent = description;
-    eventDate.textContent = date;
-
-    eventContainer.appendChild(closeButton);
-    eventContainer.appendChild(eventAuthor);
-    eventContainer.appendChild(eventName);
-    eventContainer.appendChild(eventDescription);
-    eventContainer.appendChild(eventDate);
-
-    const eventsSection = document.querySelector('.events');
-    eventsSection.appendChild(eventContainer);
-
-    closeButton.addEventListener('click', function () {
-      eventsSection.removeChild(eventContainer);
-    });
+export function hiddenFormDisplayForm() {
+  console.log("toggle button");
+  const form = document.getElementById('add-events');
+  if (form.style.display !== 'flex') {
+    console.log("if css not flex");
+    form.style.display = 'flex';
+  } else {
+    console.log("if css flex");
   }
-
-  display();
 }
-
-buttonToggle.addEventListener('click', hideFormDisplayEvent);
