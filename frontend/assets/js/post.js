dad71b9f73eb40b3283ee936ef6b283965564bd3
Steve Grard <grard.steve@gmail.com>
2023-05-31 15:05:44 +0200

    Add events are ok. Verification system to send datas in progress

--- frontend/assets/js/post.js
+++ frontend/assets/js/post.js
@@ -1,10 +1,10 @@
 export function postEvents(){
     const authorTag = document.getElementById("add-events__author")
-    const author = authorTag.value
+    const author = authorTag.value.trim()
     const nameTag = document.getElementById("add-events__name")
-    const name = nameTag.value
+    const name = nameTag.value.trim()
     const descriptionTag = document.getElementById("add-events__description")
-    const description = descriptionTag.value
+    const description = descriptionTag.value.trim()
     const dateTag = document.getElementsByClassName("add-events__date")
 
     let dates = []
@@ -12,12 +12,40 @@ export function postEvents(){
         dates.push(dateTag[i].value)
     }
 
-    formatEventObject(author, name, description, dates)
+    if(!author){
+        authorTag.classList.add("alert-field")
+        console.error('The author name must be filled');
+        return;
+    }
+    authorTag.classList.remove("alert-field")
+
+    if(!name){
+        nameTag.classList.add("alert-field")
+        console.error('The event name must be filled');
+        return;
+    }
+    nameTag.classList.remove("alert-field")
+    
+    if(!description){
+        descriptionTag.classList.add("alert-field")
+        console.error('The event description must be filled');
+        return;
+    }
+    descriptionTag.classList.remove("alert-field")
+
+    let dateNumber = 1;
+    for(let date of dates){
+        if(!date){
+            console.error(`Your dates number ${dateNumber} must be filled`);
+        }
+        dateNumber++;
+    }
+
+    formatEventObject(author, name, description, dates);
+    
 }
 
 function formatEventObject(author, name, description, dates){
-
-
     const data = {
         "name" : name,
         "dates" : dates,
@@ -29,7 +57,8 @@ function formatEventObject(author, name, description, dates){
 }
 
 function sendEventToDB(data){
-    fetch("http://localhost:3000/api/events",{
+    try{
+        fetch("http://localhost:3000/api/events",{
 
         method: "POST",
         body: JSON.stringify(data),
@@ -37,6 +66,9 @@ function sendEventToDB(data){
             "Content-type" : "application/json; charset=UTF-8"
         }
     })
-    .then(response => response.json())
-    .then(json => console.log(json))
+        .then(response => response.json())
+    }
+    catch(error){
+        console.log('There was an error', error)
+    }
 }
