b4e3ed4febff58b8d5222eff955fbd08d7b090a5
Steve Grard <grard.steve@gmail.com>
2023-05-31 14:15:38 +0200

    Buttons to remove dates functionnal. TODO : correction to add events

--- frontend/assets/js/addDate.js
+++ frontend/assets/js/addDate.js
@@ -1,10 +1,14 @@
 const addDateButton = document.querySelector('#add-events__date_button');
 const form = document.querySelector("form") 
 
-export function addDate (){
+let removeButtons
+let containerNewDateID = 0
+export function addDate(){
     addDateButton.addEventListener('click',()=>{
         const containerNewDate = document.createElement('section')
         containerNewDate.classList.add("add-events__date--container")
+        containerNewDate.setAttribute("id", `dateID-${containerNewDateID}`)
+        containerNewDateID++
 
         const addDate = document.createElement('input');
         addDate.type = "date";
@@ -17,5 +21,28 @@ export function addDate (){
         form.insertBefore(containerNewDate,addDateButton)
         containerNewDate.appendChild(addDate);
         containerNewDate.appendChild(removeDate);
+
+        listenRemoveDate();
     })
+    
+}
+
+function listenRemoveDate(){
+    removeButtons = document.getElementsByClassName("add-events__remove-date")
+    let dateToRemove
+    let buttonConcerned
+    for(let button of removeButtons){
+        dateToRemove = button.parentElement
+        buttonConcerned = button
+    }
+    buttonConcerned.addEventListener("click", event => {
+        removeDate(dateToRemove)
+    })        
+    // removeDate(dateToRemove) 
+}
+
+function removeDate(dateToRemove){
+    // const idSelected = document.getElementById(`${dateToRemove.id}`)
+    // idSelected.remove()
+    dateToRemove.remove()
 }
