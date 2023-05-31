90a527311485d0a6e9c5ac65b7cf2588e9250b7e
Steve Grard <grard.steve@gmail.com>
2023-05-31 10:56:55 +0200

    Render of the form is OK

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,7 @@
 import { renderEvents } from "./render.js";
 import { postEvents } from "./post.js";
 import { addDate } from "./addDate.js";
+import { openForm } from "./openForm.js";
 
 renderEvents();
 addDate();
@@ -10,3 +11,9 @@ submit.addEventListener("click", (event) => {
   event.preventDefault();
   postEvents();
 });
+
+const buttonForm = document.getElementById("open-form");
+const form = document.getElementById("add-events")
+buttonForm.addEventListener("click", event => {
+  openForm(buttonForm, form)
+})
